62452abd2dd3e4ad0417536cbba892c268f3c91b
diana-kk <61671420+diana-kk@users.noreply.github.com>
2020-03-03 11:34:50 +0900

    Add files via upload

---
+++ 01_la_check.js
@@ -0,0 +1,635 @@
+var SVG = Snap('#my-svg');
+
+// 최상위 그룹
+var Paper = SVG.g();
+
+//이미지
+var login = Paper.image('../img/login.png', 0, 0, 30, 45).toDefs();
+
+
+// SVG 외곽선
+Paper.rect(0, 0, 360, 640, 6).attr({
+  'stroke': 'gray',
+  'fill': 'none'
+});
+
+// 상단 바 부분
+var topBox = Paper.g();
+
+topBox.path('M1 40 L359 40 L359 0 Q358 1 358 1 L1 1').attr({
+  'fill': 'Wheat'
+});
+
+topBox.rect(11, 6, 32, 28).click(handler01).attr({
+  'fill': 'Wheat',
+  'cursor': 'pointer'
+});
+
+for (var i = 0; i < 3; i++) {
+  topBox.line(15, 13 + (i * 8), 40, 13 + (i * 8)).click(handler01).attr({
+    'stroke': 'black',
+    'cursor': 'pointer'
+  });
+}
+
+topBox.text(290, 27, '라등급').attr({
+  'font-size': 18
+});
+
+function handler01() {
+  location.replace('gub_01.html');
+}
+
+// 상단
+var topArea = Paper.g();
+login.use().transform('t30, 55').appendTo(topArea);
+
+topArea.text(68, 90, '1~5세트 학습한자').attr({
+  'font-size': 18,
+  'font-weight': 'bold'
+});
+
+topArea.rect(250, 72, 80, 25, 5).attr({
+  'fill': 'LemonChiffon',
+  'stroke': 'gray'
+});
+topArea.text(260, 89, '카드로 보기').click(handlerCard).attr({
+  'font-size': 13,
+  'cursor': 'pointer'
+});
+
+function handlerCard() {
+  location.replace('gub_02-02.html');
+}
+
+// 한자
+var bottomArea = Paper.g();
+
+for (var i = 0; i < 5; i++) {
+  bottomArea.rect(30, (i * 100) + 130, 300, 60, 5).attr({
+    'fill': '#f1f3f4'
+  });
+}
+
+var hanjaChoices = Paper.g();
+
+hanjaChoices.text(45, 170, '頁').click(handler05).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 170, '禾').click(handler06).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 170, '示').click(handler07).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 170, '弓').click(handler08).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 170, '貝').click(handler09).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 270, '未').click(handler10).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 270, '束').click(handler11).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 270, '村').click(handler12).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 270, '業').click(handler13).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 270, '校').click(handler14).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 370, '仁').click(handler15).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 370, '仙').click(handler16).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 370, '信').click(handler17).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 370, '伐').click(handler18).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 370, '住').click(handler19).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 470, '否').click(handler20).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 470, '君').click(handler21).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 470, '句').click(handler22).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 470, '同').click(handler23).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 470, '器').click(handler24).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 570, '共').click(handler25).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 570, '兵').click(handler26).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 570, '具').click(handler27).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 570, '兼').click(handler28).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 570, '公').click(handler29).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+
+// 핸들러 함수
+function handler03() {
+  location.replace('gub_01.html');
+}
+
+function handler04() {
+  location.replace('gub_03.html');
+}
+
+function handler05() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '머리 혈').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler06() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '벼 화').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler07() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '보일 시').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler08() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '활 궁').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler09() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '조개 패').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler10() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '아닐 미').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler11() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '묶을 속').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler12() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '마디 촌').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler13() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '일 업').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler14() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '학교 교').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler15() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '어질 인').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler16() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '신선 선').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler17() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '믿을 신').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler18() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '칠 벌').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler19() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '머무를 주').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler20() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '아닐 부').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler21() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '임금 군').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler22() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '글귀 구').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler23() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '한가지 동').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler24() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '그릇 기').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+
+function handler25() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '함께 공').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler26() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '병사 병').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler27() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '갖출 구').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler28() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '겸할 겸').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler29() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, '공변될 공').attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
