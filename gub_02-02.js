0542a2eb0f7562829ad3b2967ca7592dccf9fffb
diana-kk <61671420+diana-kk@users.noreply.github.com>
2020-03-03 13:13:06 +0900

    replace

--- gub_02-02.js
+++ gub_02-02.js
@@ -6,7 +6,7 @@ var Paper = SVG.g();
 
 //이미지
 
-var oji = Paper.image('../img/oji.png', 0, 0, 130, 150).toDefs();
+var oji = Paper.image('oji.png', 0, 0, 130, 150).toDefs();
 
 // SVG 외곽선
 Paper.rect(0, 0, 360, 640, 6).attr({
