fecca2ac6da94cbbcb508ecec3ff3e72c631e094
diana-kk <61671420+diana-kk@users.noreply.github.com>
2020-03-05 13:43:27 +0900

    Update end(16).js

--- end(16).js
+++ end(16).js
@@ -4,8 +4,7 @@ var SVG = Snap('#my-svg');
 var Paper = SVG.g();
 
 //이미지
-var fireworks04 = Paper.image('fireworks04.png', 0, 0, 320, 200).toDefs();
-
+var finish04 = Paper.image('finish04.png', 0, 0, 200, 200).toDefs();
 
 // SVG 외곽선
 Paper.rect(0, 0, 360, 640, 6).attr({
@@ -17,7 +16,7 @@ Paper.text(35, 160, '이번 주 학습을 마칩니다!').attr({
   'font-size': 30
 });
 
-fireworks04.use().transform('t20, 190').appendTo(Paper);
+finish04.use().transform('t80, 185').appendTo(Paper);
 
 var orangeB = Paper.rect(120, 410, 120, 30).attr({
   'fill': '#fe9b00',
