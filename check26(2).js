dbdfa8d91f12ec78c38f2436e4514533910c53eb
diana-kk <61671420+diana-kk@users.noreply.github.com>
2020-03-06 14:08:02 +0900

    Update check26(2).js

--- check26(2).js
+++ check26(2).js
@@ -4,7 +4,8 @@ var SVG = Snap('#my-svg');
 var Paper = SVG.g();
 
 //이미지
-var oji = Paper.image('duduteacher.png', 0, 0, 130, 160).toDefs();
+var login = Paper.image('dudu01.png', 0, 0, 30, 45).toDefs();
+
 
 // SVG 외곽선
 Paper.rect(0, 0, 360, 640, 6).attr({
@@ -31,25 +32,591 @@ for (var i = 0; i < 3; i++) {
   });
 }
 
-topBox.text(283, 27, '라26~30').attr({
+topBox.text(290, 27, '라등급').attr({
   'font-size': 18
 });
 
 
 // 상단
-var hanjaArea = Paper.g();
+var topArea = Paper.g();
+login.use().transform('t30, 55').appendTo(topArea);
 
-hanjaArea.text(180, 180, '한자를 보고').attr({
-  'font-size': 40,
-  'text-anchor': 'middle'
+topArea.text(68, 90, '26~30세트 학습한자').attr({
+  'font-size': 18,
+  'font-weight': 'bold'
 });
-hanjaArea.text(180, 260, '뜻음을 말해 보세요.').attr({
-  'font-size': 40,
-  'text-anchor': 'middle'
+
+topArea.rect(250, 72, 80, 25, 5).attr({
+  'fill': 'LemonChiffon',
+  'stroke': 'gray'
 });
-oji.use().transform('t110, 310').appendTo(hanjaArea);
+topArea.text(260, 89, '카드로 보기').click(handlerCard).attr({
+  'font-size': 13,
+  'cursor': 'pointer'
+});
+
+function handlerCard() {
+  location.replace('check26(2).html');
+}
+
+// 한자
+var bottomArea = Paper.g();
+
+for (var i = 0; i < 5; i++) {
+  bottomArea.rect(30, (i * 100) + 130, 300, 60, 5).attr({
+    'fill': '#f1f3f4'
+  });
+}
+
+var hanjaChoices = Paper.g();
+
+hanjaChoices.text(45, 170, data[125].hanja).click(handler05).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 170, data[126].hanja).click(handler06).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 170, data[127].hanja).click(handler07).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 170, data[128].hanja).click(handler08).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 170, data[129].hanja).click(handler09).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 270, data[130].hanja).click(handler10).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 270, data[131].hanja).click(handler11).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 270, data[132].hanja).click(handler12).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 270, data[133].hanja).click(handler13).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 270, data[134].hanja).click(handler14).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 370, data[135].hanja).click(handler15).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 370, data[136].hanja).click(handler16).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 370, data[137].hanja).click(handler17).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 370, data[138].hanja).click(handler18).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 370, data[139].hanja).click(handler19).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 470, data[140].hanja).click(handler20).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 470, data[141].hanja).click(handler21).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 470, data[142].hanja).click(handler22).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 470, data[143].hanja).click(handler23).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 470, data[144].hanja).click(handler24).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(45, 570, data[145].hanja).click(handler25).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(105, 570, data[146].hanja).click(handler26).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(165, 570, data[147].hanja).click(handler27).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(225, 570, data[148].hanja).click(handler28).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+hanjaChoices.text(285, 570, data[149].hanja).click(handler29).attr({
+  'font-size': 32,
+  'cursor': 'pointer'
+});
+
+function handler05() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[125].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler06() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[126].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler07() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[127].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler08() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[128].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler09() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[129].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler10() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[130].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler11() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[131].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler12() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[132].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler13() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[133].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler14() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[134].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler15() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[135].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler16() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[136].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler17() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[137].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler18() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[138].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler19() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[139].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler20() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[140].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler21() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[141].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler22() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[142].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler23() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[143].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler24() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[144].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
 
 
-setTimeout(function() {
-  location.replace('check26(3).html');
-}, 4000);
+function handler25() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[145].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler26() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[146].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler27() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[147].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler28() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[148].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
+
+function handler29() {
+  var group = Paper.g();
+  group.rect(50, 250, 255, 180, 5).attr({
+    'fill': '#ffc5d0',
+    'opacity': 0.9
+  });
+  group.text(180, 350, data[149].mean).attr({
+    'fill': 'black',
+    'font-size': 50,
+    'text-anchor': 'middle'
+  });
+
+  setTimeout(function() {
+    group.remove();
+  }, 1000);
+}
